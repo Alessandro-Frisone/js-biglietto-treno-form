c9c2ca39fbfc0e5896533ba62446bb26add273e5
Alessandro-Frisone <alefri2004@gmail.com>
2024-12-10 16:10:28 +0100

    fix: js calc price

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
 const name = document.getElementById("name")
-const distance = Number(document.getElementById("distance")) 
+const distance = document.getElementById("distance") 
 const age = document.getElementById("age")
 const genera = document.getElementById("genera")
 const annulla = document.getElementById("annulla")
 const priceForKm = 0.21;
 
-
 genera.addEventListener("click", function(){
-let price = distance * priceForKm;
-if (age <= 18) {
+let price = distance.value * priceForKm;
+if (age.value === "opzione2") {
   price -= price * 0.20; 
 }
- if (age >= 65){
+ if (age.value === "opzione3"){
     price -= price * 0.40;
 }
-return price;
-});
 console.log(price)
+});
